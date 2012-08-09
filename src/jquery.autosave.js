487d39cd50ec4b43c50c8432a99fc9150e7b87e6
Kyle Florence <kyle.florence@gmail.com>
2012-08-08 22:23:09 -0700

    Stub out some more methods.

--- src/jquery.autosave.js
+++ src/jquery.autosave.js
@@ -11,19 +11,36 @@ Dual licensed under the MIT and BSD Licenses.
 
 (function( $, undefined ) {
 
+var handlerTypes = [ "trigger", "scope", "data", "condition", "store" ],
+    inputChangeEvents = [ "change", "keyup" ],
+    uuid = 0;
+
+function Handler( settings ) {
+    $.extend( this, settings );
+    this.uuid = ++uuid;
+}
+
+Handler.prototype = {
+    options: {},
+    setup: function() {},
+    run: function() {},
+    teardown: function() {}
+};
+
 $.widget( "kf.autosave", {
     version: "2.0.0-rc1",
-    queues: $({}),
+
     options: {
         ignore: ":hidden",
 
+        // Triggers
         trigger: "interval",
         scope: "all",
         data: "serialize",
         condition: "changed",
-        save: "ajax",
+        store: "ajax",
 
-        // callbacks
+        // Callbacks
         change: null,
         complete: null
     },
@@ -43,15 +60,15 @@ $.widget( "kf.autosave", {
             throw "Unable to locate form!";
         }
 
-        // Listen for changes on the inputs
-        $.each( "change keyup".split( " " ), $.proxy(function( i, eventName ) {
+        $.each( inputChangeEvents, $.proxy(function( i, eventName ) {
             element.on( eventName + this.eventNamespace, ":input", $.proxy( this._change, this ) );
         }, this ));
 
-        // Set up triggers
-        // TODO
+        // Internal handlers cache
+        this._handlers = {};
     },
 
+    // FIXME: https://github.com/nervetattoo/jquery-autosave/issues/18
     _change: function( event ) {
         var target = $( event.target );
 
@@ -61,24 +78,48 @@ $.widget( "kf.autosave", {
         }
     },
 
-    _destroy: function() {
+    destroy: function() {
         this.interval();
 
         this.element
             .off( this.eventNamespace )
             .find( ":input" ).removeClass( this.widgetFullName + "-changed" );
+
+        $.Widget.prototype.destroy.call( this );
     },
 
+    // { name: "name", type: "type", options: {}, setup: function(){}, teardown: function(){} }
+    // [ handlers ]
     _resolveHandler: function( handler ) {
         var handler = {},
             type = typeof handler;
 
-        if ( type == "function" ) {
-            handler.handler
+        // TODO
     },
 
     _save: function() {
         // TODO
+    },
+
+    _setOption: function( key, value ) {
+        var handler;
+
+        if ( $.inArray( key, handlerTypes ) ) {
+
+            // TODO: promise pattern?
+            this.removeHandler( this.handlers[ key ] );
+            this.addHandler( value );
+        }
+
+        $.Widget.prototype._setOption.apply( this, arguments );
+    },
+
+    // TODO: promise pattern?
+    addHandler: function( handler ) {
+        $.each( this._resolveHandler( handler ), $.proxy(function( i, handler ) {
+            handler.setup.call( this, handler.options );
+            this._handlers[ handler.uuid ] = handler;
+        }, this ));
     },
 
     interval: function( interval, callback ) {
@@ -90,7 +131,18 @@ $.widget( "kf.autosave", {
         if ( !isNaN( parseInt( interval ) ) &&  $.isFunction( callback ) ) {
             this.timer = setTimeout( $.proxy( callback, this ), interval );
         }
+    },
+
+    // TODO: promise pattern?
+    removeHandler: function( handler ) {
+        $.each( this._resolveHandler( handler ), $.proxy(function( i, handler ) {
+            handler.teardown.call( this, handler.options );
+            delete this._handlers[ handler.uuid ];
+        }, this ));
     }
 });
+
+// Public handler cache
+$.kf.handlers = {};
 
 })( jQuery );
