926ee1697997c2dcb37851903549dc275a5410af
Kyle Florence <kyle.florence@gmail.com>
2012-08-28 23:43:08 -0700

    Finish resolveHandler functions. Some cleanup and reorganization.

--- src/jquery.autosave.js
+++ src/jquery.autosave.js
@@ -3,31 +3,46 @@ jQuery.autosave v2.0.0-rc1
 https://github.com/kflorence/jquery-autosave/
 Periodically saves form data based on a set of critera.
 
+Requires:
+ - jQuery 1.7.0+
+ - jQuery UI Widget 1.8.0+
+
 Inspired by the jQuery.autosave plugin written by Raymond Julin,
 Mads Erik Forberg and Simen Graaten.
 
-Dual licensed under the MIT and BSD Licenses.
+Copyright (C) 2012 Kyle Florence
+Dual licensed under the MIT and BSD licenses.
 */
 
 (function( $, undefined ) {
 
 var handlerTypes = [ "trigger", "scope", "data", "condition", "store" ],
     inputChangeEvents = [ "change", "keyup" ],
-    uuid = 0;
+    namespace = "kf",
+    uuid = 0,
+    widgetName = "autosave";
 
-function Handler( settings ) {
-    $.extend( this, settings );
-    this.uuid = ++uuid;
-}
+// https://gist.github.com/3507025
+var getType = (function() {
+    var rFunctionName = /function ([^(]+)/;
 
-Handler.prototype = {
-    options: {},
-    setup: function() {},
-    run: function() {},
-    teardown: function() {}
-};
+    return function( object ) {
+        var matches,
+            type = $.type( object );
 
-$.widget( "kf.autosave", {
+        if ( type == "object" ) {
+            matches = rFunctionName.exec( object.constructor.toString() );
+
+            if ( matches && matches.length > 1 ) {
+                type = matches[ 1 ].toLowerCase();
+            }
+        }
+
+        return type;
+    }
+}());
+
+$.widget( namespace + "." + widgetName, {
     version: "2.0.0-rc1",
 
     options: {
@@ -78,23 +93,49 @@ $.widget( "kf.autosave", {
         }
     },
 
-    destroy: function() {
-        this.interval();
-
-        this.element
-            .off( this.eventNamespace )
-            .find( ":input" ).removeClass( this.widgetFullName + "-changed" );
-
-        $.Widget.prototype.destroy.call( this );
+    _error: function( message ) {
+        throw new Error( "(" + widgetName + ") " + message );
     },
 
-    // { name: "name", type: "type", options: {}, setup: function(){}, teardown: function(){} }
-    // [ handlers ]
     _resolveHandler: function( handler ) {
-        var handler = {},
+        var resolved = {},
             type = typeof handler;
 
-        // TODO
+        // Shortcut for { run: function() {} }
+        if ( type == "function" ) {
+            resolved.run = handler;
+
+        // The name of a public handler
+        } else if ( type == "string" ) {
+            resolved = _handlers[ handler ];
+
+        // A Handler object or the settings for a new Handler object
+        } else if ( type == "object" ) {
+            resolved = handler;
+        }
+
+        if ( $.isPlainObject( resolved ) && $.isFunction( resolved.run ) ) {
+            resolved = new Handler( resolved );
+
+        } else if ( !resolved || !isHandler( resolved ) || !$.isFunction( resolved.run ) ) {
+            this._error( "Invalid handler" );
+        }
+
+        return resolved;
+    },
+
+    _resolveHandlers: function( handlers ) {
+        var resolved = [];
+
+        if ( !$.isArray( handlers ) ) {
+            handlers = [ handlers ];
+        }
+
+        $.each( handlers, $.proxy(function( i, handler ) {
+            resolved.push( this._resolveHandler( handler ) );
+        }, this ));
+
+        return resolved;
     },
 
     _save: function() {
@@ -106,7 +147,7 @@ $.widget( "kf.autosave", {
 
         if ( $.inArray( key, handlerTypes ) ) {
 
-            // TODO: promise pattern?
+            // TODO: use promise pattern
             this.removeHandler( this.handlers[ key ] );
             this.addHandler( value );
         }
@@ -114,12 +155,23 @@ $.widget( "kf.autosave", {
         $.Widget.prototype._setOption.apply( this, arguments );
     },
 
-    // TODO: promise pattern?
+    // TODO: use promise pattern
     addHandler: function( handler ) {
-        $.each( this._resolveHandler( handler ), $.proxy(function( i, handler ) {
-            handler.setup.call( this, handler.options );
-            this._handlers[ handler.uuid ] = handler;
+        $.each( this._resolveHandlers( handler ), $.proxy(function( i, handler ) {
+            if ( handler.setup.call( this, handler.options ) !== false ) {
+                this._handlers[ handler.uuid = ++uuid ] = handler;
+            }
         }, this ));
+    },
+
+    destroy: function() {
+        this.interval();
+
+        this.element
+            .off( this.eventNamespace )
+            .find( ":input" ).removeClass( this.widgetFullName + "-changed" );
+
+        $.Widget.prototype.destroy.call( this );
     },
 
     interval: function( interval, callback ) {
@@ -133,16 +185,39 @@ $.widget( "kf.autosave", {
         }
     },
 
-    // TODO: promise pattern?
+    // TODO: use promise pattern
     removeHandler: function( handler ) {
         $.each( this._resolveHandler( handler ), $.proxy(function( i, handler ) {
-            handler.teardown.call( this, handler.options );
-            delete this._handlers[ handler.uuid ];
+            if ( handler.teardown.call( this, handler.options ) !== false ) {
+                delete this._handlers[ handler.uuid ];
+            }
         }, this ));
     }
 });
 
 // Public handler cache
-$.kf.handlers = {};
+var _handlers = $[ namespace ][ widgetName ][ "handlers" ] = {};
+
+function isHandler( object ) {
+    return getType( object ) == "handler";
+}
+
+function Handler( settings ) {
+
+    // Allow calling without the 'new' operator
+    if ( !isHandler( this ) ) {
+        return new Handler( settings );
+    }
+
+    $.extend( this, settings );
+}
+
+Handler.prototype = {
+    constructor: Handler,
+    options: {},
+    run: function() {},
+    setup: function() {},
+    teardown: function() {}
+};
 
 })( jQuery );
