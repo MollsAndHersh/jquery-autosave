25a61c24070c50fc181767697c39599836daf00d
Kyle Florence <kyle.florence@gmail.com>
2012-10-14 19:24:26 -0700

    Remove handler types. Move Handler class into its own file.

--- src/jquery.autosave.js
+++ src/jquery.autosave.js
@@ -16,11 +16,9 @@ Dual licensed under the MIT and BSD licenses.
 
 (function( $, undefined ) {
 
-var handlers,
+var publicHandlers,
     classNames = [ "change" ],
-    handlerTypes = [ "trigger", "scope", "data", "condition", "store" ],
-    handlerTypesLength = handlerTypes.length,
-    inputChangeEvents = [ "change", "keyup" ],
+    eventNames = [ "change", "keyup" ],
     namespace = "kf",
     uuid = 0,
     widgetName = "autosave";
@@ -31,46 +29,15 @@ var arr = function( obj ) {
     return $.isArray( obj ) ? obj : [ obj ];
 };
 
-// https://gist.github.com/3507025
-var getType = (function() {
-    var rFunctionName = /function ([^(]+)/;
-
-    return function( object ) {
-        var matches,
-            type = $.type( object );
-
-        if ( type == "object" ) {
-            matches = rFunctionName.exec( object.constructor.toString() );
-
-            if ( matches && matches.length > 1 ) {
-                type = matches[ 1 ].toLowerCase();
-            }
-        }
-
-        return type;
-    }
-}());
-
-var isHandler = function( object ) {
-    return getType( object ) == "handler";
-};
-
-var whenArray = function( deferreds ) {
-    return $.when.apply( null, deferreds );
-};
-
 $.widget( namespace + "." + widgetName, {
     version: "2.0.0-rc1",
 
-    options: {
-        ignore: ":hidden",
+    classNames: {},
+    handlers: {},
 
-        // Handlers
-        trigger: "interval",
-        scope: "all",
-        data: "serialize",
-        condition: "changed",
-        store: "ajax",
+    options: {
+        handler: null,
+        ignore: ":hidden",
 
         // Callbacks
         change: $.noop,
@@ -78,52 +45,32 @@ $.widget( namespace + "." + widgetName, {
         ready: $.noop
     },
 
-    handlers: {},
-    handlersByName: {},
-    handlersByType: (function() {
-        var handlerType,
-            handlersByType = {},
-            i = handlerTypesLength;
-
-        while ( ( handlerType = handlerTypes[ --i ] ) ) {
-            handlersByType[ handlerType ] = {};
-        }
-
-        return handlersByType;
-    })(),
-
-    _classNames: {},
-
     _create: function() {
-        var form,
-            self = this,
-            deferreds = [],
-            element = self.element;
+        var className, form, i, length,
+            element = this.element,
+            handler = this.options.handler || this.options.handlers;
 
+        // Try to find the form associated the given element
         if ( element.is( "form" ) ) {
-            form = element;
+            this.form = element;
 
-        } else if ( !( form = element.find( "form" ) ).length ) {
-            form = element.closest( "form" );
+        } else if ( !( this.form = element.find( "form" ) ).length ) {
+            this.form = element.closest( "form" );
         }
 
-        if ( !( self.form = form ).length ) {
-            self._error( "Unable to locate form" );
+        // Cache class names with widget full name
+        for ( i = 0, length = classNames.length; i < length; i++ ) {
+            className = classNames[ i ];
+            this.classNames[ className ] = this.widgetFullName + "-" + className;
         }
 
-        $.each( classNames, function( i, className ) {
-            self._classNames[ className ] = self.widgetFullName + "-" + className;
-        });
+        // Listen for changes to inputs inside of element
+        for ( i = 0, length = eventNames.length; i < length; i++ ) {
+            eventName = eventNames[ i ];
+            element.on( eventName + this.eventNamespace, ":input", $.proxy( this._change, this ) );
+        }
 
-        $.each( inputChangeEvents, function( i, eventName ) {
-            element.on( eventName + self.eventNamespace, ":input", $.proxy( self._change, self ) );
-        });
-
-        $.each( handlerTypes, function( i, type ) {
-            deferreds.push( self.addHandler( type, self.options[ type ] ) );
-        });
-
-        whenArray( deferreds ).done( self.options.ready );
+        this.addHandler( handler ).done( this.options.ready );
     },
 
     // FIXME: https://github.com/nervetattoo/jquery-autosave/issues/18
@@ -131,7 +78,7 @@ $.widget( namespace + "." + widgetName, {
         var target = $( event.target );
 
         if ( !target.hasClass( this.options.ignore ) ) {
-            target.addClass( this._classNames.change );
+            target.addClass( this.classNames.change );
             this.element._trigger( "change", event );
         }
     },
@@ -140,42 +87,36 @@ $.widget( namespace + "." + widgetName, {
         throw new Error( "(" + widgetName + ") " + message );
     },
 
-    _resolveHandler: function( type, handler ) {
-        var typeOf,
-            resolved = [],
-            self = this;
+    _resolveHandler: function( handler ) {
+        var handlers, i, length, typeOf,
+            resolved = [];
 
         if ( !handler ) {
             return resolved;
         }
 
-        $.each( arr( handler ), function( i, handler ) {
+        handlers = arr( handler );
+
+        for ( i = 0, length = handlers.length; i < length; i++ ) {
+            handler = handlers[ i ];
             typeOf = typeof handler;
 
             if ( typeOf == "function" ) {
                 handler = { run: handler };
 
             } else if ( typeOf == "string" ) {
-                handler = handlers[ handler ];
+                handler = publicHandlers[ handler ];
             }
 
             if ( typeof handler != "object" ) {
-                self._error( "Unable to resolve Handler" );
+                this._error( "Unable to resolve Handler" );
 
-            } else {
-                handler.type = handler.type || type;
-
-                if ( $.inArray( handler.type, handlerTypes ) < 0 ) {
-                    self._error( "Invalid Handler type: " + handler.type );
-                }
-            }
-
-            if ( !isHandler( handler ) ) {
+            } else if ( !Handler.isHandler( handler ) ) {
                 handler = new Handler( handler );
             }
 
             resolved.push( handler );
-        });
+        }
 
         return resolved;
     },
@@ -188,31 +129,31 @@ $.widget( namespace + "." + widgetName, {
         $.Widget.prototype._setOption.apply( this, arguments );
     },
 
-    _updateHandler: function( type, handler ) {
-        // TODO: remove all previous handlers of same type
-        // TODO: add all new handlers for same type
+    _updateHandler: function( handler ) {
+        // TODO
     },
 
-    addHandler: function( type, handler ) {
-        var handlers,
-            deferreds = [],
+    addHandler: function( handler ) {
+        var i, length,
+            chain = new $.Deferred(),
+            handlers = this._resolveHandler( handler ),
+            promise = chain,
             self = this;
 
-        // args: handler
-        if ( arguments.length == 1 ) {
-            success = handler;
-            handler = type;
-            type = undefined;
+        for ( i = 0, length = handlers.length; i < length; i++ ) {
+            handler = handlers[ i ];
+
+            promise = promise.pipe(function() {
+                return handler.setup( handler.options );
+
+            }).done(function() {
+                self.handlers[ handler.uuid ] = handler;
+            });
         }
 
-        // TODO: handle namespaced types (like "my.trigger")
-        $.each( this._resolveHandler( type, handler ), function( i, handler ) {
-            deferreds.push( handler.setup.call( self, handler.options ) );
-            self.handlers[ handler.uuid ] = handler;
-            self.handlersByType[ handler.type ][ handler.uuid ] = handler;
-        });
+        chain.resolve();
 
-        return whenArray( deferreds );
+        return promise;
     },
 
     destroy: function() {
@@ -243,75 +184,47 @@ $.widget( namespace + "." + widgetName, {
         // TODO
     },
 
-    save: function( event, inputs, caller ) {
-        var args, argsLength, chain, deferred, handler, handlers,
-            deferreds = [],
-            index = 1,
-            lastIndex = 0,
-            self = this;
+    save: function( event, inputs ) {
+        var handler,
+            chain = new $.Deferred(),
+            deferred = new $.Deferred(),
+            promise = chain;
 
-        inputs = inputs ? $( inputs ).filter( ":input" ) : self.inputs();
+        // args: inputs
+        if ( !( event instanceof $.Event ) ) {
+            inputs = event;
+            event = undefined;
+        }
 
-        // inputs, data, caller
-        args = [ inputs, {}, caller ];
+        for ( handler in this.handlers ) {
+            handler = this.handlers[ handler ];
 
-        (function next() {
-            deferred = new $.Deferred();
-            handlers = self.handlersByType[ handlerTypes[ index ] ];
+            promise = promise.pipe(function( data ) {
+                return handler.run( handler.options, data ) || data;
 
-            for ( handler in handlers ) {
-                handler = handlers[ handler ];
-                chain = ( chain ? chain : deferred ).pipe(function() {
-                    return handler.run.apply( null, [ handler.options || {} ].concat( args.slice( 0, index ) ) );
-                });
-            }
-
-            chain.done(function( result ) {
-                if ( result === false ) {
-                    return;
-
-                } else if ( result !== undefined ) {
-                    args[ lastIndex ] = result;
-                }
-
-                lastIndex = index;
-
-                if ( ++index < handlerTypesLength ) {
-                    next();
-
-                } else {
-                    self._trigger( "save", event, arguments );
-                    self.inputs().removeClass( self._classNames.change );
-                }
+            // If any promise fails, reject deferred
+            }).fail(function( data ) {
+                deferred.reject( data );
             });
+        }
 
-            deferred.resolve( args.slice( lastIndex, index ) );
-        })();
+        // Resolve deferred when the last promise is done
+        promise.done(function( data ) {
+            deferred.resolve( data );
+        });
+
+        // Start the chain
+        chain.resolve({
+            data: {},
+            event: event,
+            inputs: inputs ? $( inputs ).filter( ":input" ) : this.inputs()
+        });
+
+        return deferred.promise();
     }
 });
 
-// Public handlers
-$[ namespace ][ widgetName ][ "handlers" ] = handlers = {};
-
-// Handler Class
-function Handler( settings ) {
-
-    // Allow calling without the 'new' operator
-    if ( !isHandler( this ) ) {
-        return new Handler( settings );
-    }
-
-    $.extend( this, settings );
-
-    this.uuid = uuid++;
-}
-
-Handler.prototype = {
-    constructor: Handler,
-    options: {},
-    run: $.noop,
-    setup: $.noop,
-    teardown: $.noop
-};
+// Public handlers namespace
+$[ namespace ][ widgetName ][ "handlers" ] = publicHandlers = {};
 
 })( jQuery );
