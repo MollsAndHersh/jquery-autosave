ffd12a734564190fc2dfef53ce3b1363e268ea50
Kyle Florence <kyle.florence@gmail.com>
2013-12-31 16:04:01 -0800

    Cleanup; spaces to tabs

--- test/unit/core.js
+++ test/unit/core.js
@@ -1,27 +1,29 @@
 (function( $, undefined ) {
-    module( "Core" );
 
-    test( "Requirements", function() {
-        expect( 4 );
+	module( "Core" );
 
-        ok( jQuery, "jQuery exists" );
-        ok( $, "$ exists" );
-        ok( $.Autosave, "$.Autosave exists" );
-        ok( $.fn.autosave, "$.fn.Autosave exists" );
-    });
+	test( "Requirements", function() {
+		expect( 5 );
 
-    test( "Initialization / Destruction", function() {
-        expect( 3 );
+		ok( $, "jQuery exists" );
+		ok( $.Autosave, "jQuery.Autosave exists" );
+		ok( $.Autosave.Handler, "jQuery.Autosave.Handler exists" );
+		ok( $.Deferred.Sequence, "jQuery.Deferred.Sequence" );
+		ok( $.fn.autosave, "jQuery.fn.Autosave exists" );
+	});
 
-        var fixture = $( "#qunit-fixture" ).autosave(),
-            autosave = fixture.data( 'autosave' );
+	test( "Initialization / Destruction", function() {
+		expect( 3 );
 
-        ok( autosave instanceof jQuery.Autosave, "Autosave instance stored in element." );
-        equal( autosave.inputs().length, 11, "Element contains 11 inputs (one is ignored)." );
+		var fixture = $( "#qunit-fixture" ).autosave(),
+			autosave = fixture.data( 'autosave' );
 
-        autosave.destroy();
+		ok( autosave instanceof jQuery.Autosave, "Autosave instance stored in element." );
+		equal( autosave.inputs().length, 11, "Element contains 11 inputs (one is ignored)." );
 
-        equal( fixture.data( "autosave" ), undefined, "Autosave instance has been destroyed." );
-    });
+		autosave.destroy();
+
+		equal( fixture.data( "autosave" ), undefined, "Autosave instance has been destroyed." );
+	});
 
 })( jQuery );
