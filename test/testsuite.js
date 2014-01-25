cd7b0dc50e0aaaf0099eefca9b8b520aa7b183f3
Kyle Florence <kyle.florence@gmail.com>
2014-01-24 23:04:19 -0800

    Fix running QUnit tests in PhantomJS
    Apparently loading QUnit with RequireJS doesn't work very well. Very
    lame. https://twitter.com/jrburke/statuses/158020046705278977

--- test/testsuite.js
+++ test/testsuite.js
@@ -1,28 +1,20 @@
 (function() {
+	var source = [
+			'jquery-bridge'
+		],
+		units = [
+			'unit/autosave'
+		];
+
 	require.config({
 		baseUrl: '../src',
 		paths: {
 			jquery: '../bower_components/jquery/jquery',
-			qunit: '../bower_components/qunit/qunit/qunit',
 			unit: '../test/unit'
-		},
-		shim: {
-			qunit: {
-				exports: 'QUnit'
-			}
 		}
 	});
 
-	require( [ 'qunit' ], function( QUnit ) {
-		var source = [
-				'jquery-bridge'
-			],
-			units = [
-				'unit/core',
-				'unit/autosave'
-			];
-
-		// Defer QUnit test runner until all tests have loaded
-		require( source.concat( units ), QUnit.load );
-	});
+	// Defer QUnit test runner until all tests have loaded
+	QUnit.config.autostart = false;
+	require( source.concat( units ), QUnit.start );
 })();
